056292c1a2aa6c4f9a3f42bb925ad2268a9f6670
Aleksandr Kravets <aleksandrkravets@aleksandrs-macbook-air.local>
2018-08-27 15:50:09 +0300

    Initial Commit

---
+++ Fill in the gap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fill in the gap
+//
+//  Created by Aleksandr Kravets on 8/27/18.
+//  Copyright © 2018 Aleksandr Kravets. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
